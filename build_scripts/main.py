28ad24fa1a58f7232fa6bf552feb4c3544b1e561
Sophie Brun <sophie@freexian.com>
2019-11-05 10:39:14 +0100

    Drop git requirement
    Last-Update: 2018-07-06
    
    Only to allow local build without git.
    Last-Update: 2018-07-06
    
    Gbp-Pq: Name drop-git-requirement.patch

--- build_scripts/main.py
+++ build_scripts/main.py
@@ -299,9 +299,9 @@ def get_qt_version():
 
 
 def prepare_build():
-    if (os.path.isdir(".git") and not OPTION_IGNOREGIT and
-            not OPTION_ONLYPACKAGE and not OPTION_REUSE_BUILD):
-        prepare_sub_modules()
+#    if (os.path.isdir(".git") and not OPTION_IGNOREGIT and
+#            not OPTION_ONLYPACKAGE and not OPTION_REUSE_BUILD):
+#        prepare_sub_modules()
     # Clean up temp build folder.
     for n in ["build"]:
         d = os.path.join(setup_script_dir, n)
