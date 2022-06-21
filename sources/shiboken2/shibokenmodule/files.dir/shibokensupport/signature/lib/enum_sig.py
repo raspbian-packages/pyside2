d060c1bd8c7e62f0c819ed15c82475b353b3df02
Christian Tismer <tismer@stackless.com>
2022-06-21 10:22:04 +0200

    PyEnum: make forgiving duplicates work with Python 3.11
    There was a silent change in PyEnums that turns Enum attributes
    into properties. This does not harm the Python interface but
    needed some change in the duplication emulation.
    
    Furthermore, new internal enums are created with an underscore name.
    
    The meta class was changed from EnumMeta to EnumType.
    
    [ChangeLog][shiboken6] The new Python Enums are now compatible with Python 3.11
    
    Change-Id: I3b1ab63dc5eed15a75ebd0f42dddf4001f640c00
    Pick-to: 6.3
    Task-number: PYSIDE-1735
    Fixes: PYSIDE-1960
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>
    (cherry picked from commit da2cf031521815a9559ca784beadb70c7a2852d9)
    
    Gbp-Pq: Name PyEnum-make-forgiving-duplicates-work-with-Python-3.11.patch

--- sources/shiboken2/shibokenmodule/files.dir/shibokensupport/signature/lib/enum_sig.py
+++ sources/shiboken2/shibokenmodule/files.dir/shibokensupport/signature/lib/enum_sig.py
@@ -139,7 +139,8 @@ class ExactEnumerator(object):
                     functions.append((func_name, thing))
             elif type(type(thing)) is EnumMeta:
                 # take the real enum name, not what is in the dict
-                enums.append((thing_name, type(thing).__qualname__, thing))
+                if not thing_name.startswith("_"):
+                    enums.append((thing_name, type(thing).__qualname__, thing))
         init_signature = getattr(klass, "__signature__", None)
         enums.sort(key=lambda tup: tup[1 : 3])  # sort by class then enum value
         self.fmt.have_body = bool(subclasses or functions or enums or init_signature)
