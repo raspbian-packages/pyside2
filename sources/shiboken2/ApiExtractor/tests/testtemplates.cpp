76b73514b8d40921a4a73aa0e90d2a57b92e4949
Friedemann Kleint <Friedemann.Kleint@qt.io>
2023-04-25 14:01:45 +0200

    shiboken2/clang: Fix build with clang 16
    clang 16 returns more elaborated types instead of fully qualified type
    names. Qualify elaborated types when retrieving the type name.
    
    [ChangeLog][shiboken6] Support for libclang version 16 has been added.
    
    Task-number: PYSIDE-2288
    Pick-to: 6.5 5.15
    Change-Id: Ibd428280180967f11d82a72159e744c016afc927
    Reviewed-by: Christian Tismer <tismer@stackless.com>
    (cherry picked from commit 44ef1859214c66861a251d4a0faf5c38dc050850)
    
    Gbp-Pq: Name shiboken2-clang-Fix-build-with-clang-16.patch

--- sources/shiboken2/ApiExtractor/tests/testtemplates.cpp
+++ sources/shiboken2/ApiExtractor/tests/testtemplates.cpp
@@ -236,7 +236,6 @@ struct List {
     const AbstractMetaFunction *erase = list->findFunction(QStringLiteral("erase"));
     QVERIFY(erase);
     QCOMPARE(erase->arguments().size(), 1);
-    QEXPECT_FAIL("", "Clang: Some other code changes the parameter type", Abort);
     QCOMPARE(erase->arguments().at(0)->type()->cppSignature(), QLatin1String("List::Iterator"));
 }
 
@@ -389,7 +388,7 @@ typedef BaseTemplateClass<TypeOne> TypeOneClass;
     const ComplexTypeEntry* oneType = one->typeEntry();
     const ComplexTypeEntry* baseType = base->typeEntry();
     QCOMPARE(oneType->baseContainerType(), baseType);
-    QCOMPARE(one->baseClassNames(), QStringList(QLatin1String("BaseTemplateClass<TypeOne>")));
+    QCOMPARE(one->baseClassNames(), QStringList(QLatin1String("NSpace::BaseTemplateClass<NSpace::TypeOne>")));
 
     QVERIFY(one->hasTemplateBaseClassInstantiations());
     AbstractMetaTypeList instantiations = one->templateBaseClassInstantiations();
